957bdbec886f41e505ea3e6c8e09fc7f57cb6076
bwdwyer499 <71683118+bwdwyer499@users.noreply.github.com>
2022-08-17 04:30:40 +1000

    Updates

--- stage1Tests1.bat
+++ stage1Tests1.bat
@@ -1,7 +1,7 @@
-.\Release\Stage1.exe -runs 1 -threads 8 -output Outputs/stage1_01.bmp -input Scenes/all.txt -size 1024 1024 -samples 1
-.\Release\Stage1.exe -runs 1 -threads 8 -output Outputs/stage1_02.bmp -input Scenes/all.txt -size 1024 1024 -samples 4
-.\Release\Stage1.exe -runs 1 -threads 8 -output Outputs/stage1_04.bmp -input Scenes/cornell.txt -size 1024 1024 -samples 1
-.\Release\Stage1.exe -runs 1 -threads 8 -output Outputs/stage1_06.bmp -input Scenes/sponge.txt -size 128 128 -samples 1
+.\Release\Stage1.exe -runs 3 -threads 8 -output Outputs/stage1_01.bmp -input Scenes/all.txt -size 1024 1024 -samples 1
+.\Release\Stage1.exe -runs 3 -threads 8 -output Outputs/stage1_02.bmp -input Scenes/all.txt -size 1024 1024 -samples 4
+.\Release\Stage1.exe -runs 3 -threads 8 -output Outputs/stage1_04.bmp -input Scenes/cornell.txt -size 1024 1024 -samples 1
+.\Release\Stage1.exe -runs 3 -threads 8 -output Outputs/stage1_06.bmp -input Scenes/sponge.txt -size 128 128 -samples 1
 									  
 magick compare -metric mae Outputs\stage1_01.bmp Outputs_REFERENCE\all.txt_1024x1024x1_RaymarcherAss1.exe.bmp       Outputs\stage1diff_01.bmp
 magick compare -metric mae Outputs\stage1_02.bmp Outputs_REFERENCE\all.txt_1024x1024x4_RaymarcherAss1.exe.bmp       Outputs\stage1diff_02.bmp
--- stage1Tests2.bat
+++ stage1Tests2.bat
@@ -1,11 +1,11 @@
-.\Release\Stage1.exe -runs 1 -threads 8 -output Outputs/stage1_03.bmp -input Scenes/all.txt -size 500 300 -samples 1
-.\Release\Stage1.exe -runs 1 -threads 8 -output Outputs/stage1_05.bmp -input Scenes/5000spheres.txt -size 240 135 -samples 1
+.\Release\Stage1.exe -runs 1 -threads 8 -output Outputs/stage1_03.bmp -input Scenes/all.txt -size 500 300 -samples 1 -debugProgress
+.\Release\Stage1.exe -runs 1 -threads 8 -output Outputs/stage1_05.bmp -input Scenes/5000spheres.txt -size 240 135 -samples 1 -debugProgress
 
-.\Release\Stage1.exe -runs 1 -threads 3   -input Scenes/all.txt -size 1024 1024 -samples 1   -output Outputs/all.txt_1024x1024x1_Stage1.exe_3.bmp
-.\Release\Stage1.exe -runs 1 -threads 5   -input Scenes/all.txt -size 1024 1024 -samples 1   -output Outputs/all.txt_1024x1024x1_Stage1.exe_5.bmp
-.\Release\Stage1.exe -runs 1 -threads 6   -input Scenes/all.txt -size 1024 1024 -samples 1   -output Outputs/all.txt_1024x1024x1_Stage1.exe_6.bmp
-.\Release\Stage1.exe -runs 1 -threads 7   -input Scenes/all.txt -size 1024 1024 -samples 1   -output Outputs/all.txt_1024x1024x1_Stage1.exe_7.bmp
-.\Release\Stage1.exe -runs 1 -threads 513 -input Scenes/all.txt -size 1024 1024 -samples 1   -output Outputs/all.txt_1024x1024x1_Stage1.exe_513.bmp
+.\Release\Stage1.exe -runs 1 -threads 3   -input Scenes/all.txt -size 1024 1024 -samples 1 -debugProgress  -output Outputs/all.txt_1024x1024x1_Stage1.exe_3.bmp 
+.\Release\Stage1.exe -runs 1 -threads 5   -input Scenes/all.txt -size 1024 1024 -samples 1 -debugProgress  -output Outputs/all.txt_1024x1024x1_Stage1.exe_5.bmp
+.\Release\Stage1.exe -runs 1 -threads 6   -input Scenes/all.txt -size 1024 1024 -samples 1 -debugProgress  -output Outputs/all.txt_1024x1024x1_Stage1.exe_6.bmp
+.\Release\Stage1.exe -runs 1 -threads 7   -input Scenes/all.txt -size 1024 1024 -samples 1 -debugProgress  -output Outputs/all.txt_1024x1024x1_Stage1.exe_7.bmp
+.\Release\Stage1.exe -runs 1 -threads 513 -input Scenes/all.txt -size 1024 1024 -samples 1 -debugProgress  -output Outputs/all.txt_1024x1024x1_Stage1.exe_513.bmp
 
 magick compare -metric mae Outputs\stage1_03.bmp Outputs_REFERENCE\all.txt_500x300x1_RaymarcherAss1.exe.bmp         Outputs\stage1diff_03.bmp
 magick compare -metric mae Outputs\stage1_05.bmp Outputs_REFERENCE\5000spheres.txt_240x135x1_RaymarcherAss1.exe.bmp Outputs\stage1diff_05.bmp
